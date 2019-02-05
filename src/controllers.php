f3a75b05523fc61f2cf07ef72c61ad3cd772750b
Arthur Lemaire <themastertutur@gmail.com>
2019-02-05 22:14:37 +0100

    Ajout de la connexion

--- src/controllers.php
+++ src/controllers.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
@@ -13,13 +13,35 @@ $app->get('/', function () use ($app) {
 })
 ->bind('homepage')
 ;
+
+$app->get('/connexion', function () use ($app) {
+    return $app['twig']->render('connexion.html.twig');
+})
+->bind('connexion')
+;
+
+$app->post('/checklogin', function (Request $request) use ($app) {
+    $messageModel = new messageModels();
+    $isValide = $messageModel->checkIfExist($app,$request->get('name'),$request->get('password'));
+    if($isValide){
+        $app['session']->set('user', array('username' => "test"));
+        $subRequest = Request::create('/affichage');
+        return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+    }
+    $subRequest = Request::create('/connexion');
+    return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST, false);
+        }
+    );
+
 $app->get('/affichage', function () use ($app) {
     $messageModel = new messageModels();
     $message = $messageModel->getMessages($app);
+        var_dump($app['session']->get('user'));
     return $app['twig']->render('blog.html.twig', array('listemessage'=> $message));
 })
 ->bind('affichage')
 ;
+
 $app->post('/ajoutmessage', function(Request $request) use ($app) {
     $message = $request->get('message');
     $messageModel = new messageModels();
@@ -28,18 +50,16 @@ $app->post('/ajoutmessage', function(Request $request) use ($app) {
 })
 ->bind('formulairemessage')
 ;
+
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
         return;
     }
-
-    // 404.html, or 40x.html, or 4xx.html, or error.html
     $templates = array(
         'errors/'.$code.'.html.twig',
         'errors/'.substr($code, 0, 2).'x.html.twig',
         'errors/'.substr($code, 0, 1).'xx.html.twig',
         'errors/default.html.twig',
     );
-
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
 });
--- src/messageModels.php
+++ src/messageModels.php
@@ -1,6 +1,6 @@
 <?php
 use Doctrine\DBAL\Connection;
-
+use Doctrine\DBAL\Query\QueryBuilder;
 class messageModels {
     public function getMessages($app){
         $sql = "SELECT informations.* FROM informations";
@@ -12,4 +12,12 @@ class messageModels {
             'auteur' => $auteur
         ));
     }
-}
+    public function checkIfExist($app,$name,$password){
+        $sql = "SELECT * FROM utilisateur where utilisateur.nom = :nom and utilisateur.mdp = :mdp ";
+        $tmp = $app['db']->fetchAll($sql,array(':nom' => $name,'mdp' =>$password));
+        if(!empty($tmp) ){
+            return true;
+        }
+            return false;
+        }
+    }
